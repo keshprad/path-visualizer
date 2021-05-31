010a302b67f74c06ea0781957c1e1156a7359fac
keshprad <32313895+keshprad@users.noreply.github.com>
2021-05-30 21:05:16 -0700

    add navbar component

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,28 @@
 <script>
 	import { MaterialApp } from 'svelte-materialify';
-	
-	export let name;
+	import Footer from './components/Footer.svelte';
+	import Navbar from './components/Navbar.svelte';
+
+	let theme = 'dark';
 </script>
 
-<MaterialApp>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<MaterialApp {theme} style="min-height: 100vh;">
+	<Navbar />
+	<div class="grid">
+		<h1>Hello World!</h1>
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	</div>
+	<Footer />
 </MaterialApp>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0;
-	}
-
+	/* div.grid {
+		height: 100%;
+	} */
 	h1 {
 		color: #ff3e00;
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
 </style>
