185204c0003a08c05477b6267696f17df722aed7
keshprad <32313895+keshprad@users.noreply.github.com>
2021-05-30 20:05:05 -0700

    create footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { Footer } from 'svelte-materialify';
+    
+    const links = ['Home', 'About', 'Team', 'Services', 'Contact Us'];
+</script>
+
+<Footer class="justify-center flex-column">
+    <div class="pa-2 text-center" style="width:100%">
+        <p class="ma-2">
+            <strong>Path Visualizer</strong> by <a href="https://keshprad.ml">keshprad</a>.
+        </p>
+        <p class="ma-2">
+            View the source code on <a href="https://github.com/keshprad/path-visualizer" target="_blank">GitHub</a>.
+        </p>
+    </div>
+</Footer>
+
+<style>
+
+</style>
